d1805bdb92b7cfd8a0f275adc92f24a68b647e60
Christian Burroughs <christianjburroughs@me.com>
2025-02-14 15:27:53 -0600

    add digimon model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from main_app.models import Cat, Feeding, Toy
+from main_app.models import Digimon
 
 # Register your models here.
 
-admin.site.register(Cat)
-admin.site.register(Feeding)
-admin.site.register(Toy)
+admin.site.register(Digimon)
