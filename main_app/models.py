4d54ac340a444239e5b7040496d8fbb54d4bfc2a
Christian Burroughs <christianjburroughs@me.com>
2025-02-18 16:37:18 -0600

    add digifarm

--- main_app/models.py
+++ main_app/models.py
@@ -1,19 +1,35 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 
 # Create your models here.
 class Digimon(models.Model):
-  name = models.CharField(max_length=250, unique=True)
-  img = models.CharField(max_length=250)
-  level = models.TextField(max_length=250)
+  name = models.CharField(max_length=260, unique=True)
+  img = models.CharField(max_length=260)
+  level = models.TextField(max_length=260)
   happiness = models.IntegerField()
   # Create a cat >--< Toy relationship
-  user = models.ManyToManyField('Digimon')
+  user = models.ManyToManyField(User, related_name='digimon')
   # User --< Digimon
   # user = models.ForeignKey(User, on_delete=models.CASCADE)
+  # def clean(self):
+  #   """Ensure a user cannot have more than 6 Digimon."""
+  #   for user in self.user.all():
+  #     if user.digimon.count() >= 6:
+  #       raise ValidationError(f"{user.username} cannot have more than 6 Digimon.")
+  # def save(self, *args, **kwargs):
+  #   """Call clean() before saving to enforce validation."""
+  #   self.clean()
+  #   super().save(*args, **kwargs)
+  # def add_user(self, user):
+  #   """Custom method to add a user while enforcing the limit."""
+  #   if user.digimon.count() >= 6:
+  #     raise ValidationError(f"{user.username} cannot have more than 6 Digimon.")
+  #   self.user.add(user)  
+  
   
   def __str__(self):
     return f"{self.name} ({self.id})"
@@ -26,6 +42,8 @@ class Digimon(models.Model):
     # Use the 'reverse' function to dynamically find URL for 
     #   viewing this cat's details
     return reverse('digimon-index', kwargs={'digimon_id': self.id })
+  
+ 
   
   
 # class Feeding(models.Model):
